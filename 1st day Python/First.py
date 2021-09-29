4a8ae30f07a311ecb0b128a81be28b9d1ba90af0
Hyung Yun Kim <rlaguddbs97@naver.com>
2021-09-29 22:53:39 +0900

    매일 하루 1시간 파이썬 기본 문법부터 정리

--- 1st day Python/First.py
+++ 1st day Python/First.py
@@ -1,4 +1,24 @@
 a=10
-print(a) # a라는 공간에 10 변수 설정 이후 print 출력
+# print(a) a라는 변수에 숫자 10 설정 이후 print 출력
+
+b=20 # B라는 변수에 숫자 20 설정 
+
+sum = a+b #sum이라는 변수에 a+b값 저장함
 
 
+print(sum) #sum값 출력
+
+div = a/b
+
+print(div) # div이라는 변수에 a와 b값을 나눈것 저장
+
+city = "Seoul" # city라는 변수에 Seoul을 넣고 출력 
+
+print(city)
+
+List = ['아침', '점심', '저녁'] # List라는 리스트안에 아침 점심 저녁 저장
+
+print(List) # 아침, 점심, 저녁을 저장한 리스트 전부 출력
+print(List[0]) # 리스트 중 첫번째 저장값 출력 0부터 시작
+print(List[1]) # 리스트 중 두번째 저장값 출력
+print(List[2]) # 리스트 중 세번째 저장값 출력
