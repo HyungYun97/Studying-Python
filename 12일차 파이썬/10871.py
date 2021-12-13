d080d30f18750f9f880284bd13dce0be7974127d
Hyung Yun Kim <rlaguddbs97@naver.com>
2021-12-14 00:33:12 +0900

    10871 파이썬 백준

--- 12일차 파이썬/10871.py
+++ 12일차 파이썬/10871.py
@@ -1,6 +1,6 @@
-N, X = map(int,input().split())
-A = list(map(int,input().split()))
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
 for i in range(N):
-    if A[i] < N:
+    if A[i] < X:
         print(A[i], end=" ") # 인터넷 도움 받음 for if문까지는 생각했는데 세세한 문법 정리가 부족했음.
         # end="~~"를 해주게되면 줄 바꿈으로 넘어가지않고 이어서 진행하게 됨
