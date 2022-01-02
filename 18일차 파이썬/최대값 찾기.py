050428a31c558f1ba865e1bfbda1a17f386b1857
Hyung Yun Kim <rlaguddbs97@naver.com>
2022-01-02 21:40:12 +0900

    간단한 최대값 찾기

--- 18일차 파이썬/최대값 찾기.py
+++ 18일차 파이썬/최대값 찾기.py
@@ -2,9 +2,12 @@ input = [3, 5, 6, 1, 2, 4]
 
 
 def find_max_num(array):
-    
-    return max(array)
-
+    for num in array:
+        for compare_num in array:
+            if num < compare_num:
+                break
+        else:
+            return num
 
 result = find_max_num(input)
 print(result)
