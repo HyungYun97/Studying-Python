d2b77fb4323acd9bc63944c9e8b247e73e0ad8f1
Hyung Yun Kim <rlaguddbs97@naver.com>
2022-01-04 23:31:41 +0900

    점근 표기법 예시

--- 19일차 파이썬/asymptotic notation.py
+++ 19일차 파이썬/asymptotic notation.py
@@ -2,10 +2,10 @@ input = [3, 5, 6, 1, 2, 4]
 
 
 def is_number_exist(number, array):
-    for i in array:
-        if number == i:
-            return True
-    return False
+        for i in array: # array의 길이만큼 for문 실행
+            if number == i: # 비교 연산 1번 실행
+                return True # N * 1 = N 만큼
+        return False #만약 input이 4가먼저 3이 마지막일경우 for문을 다 돌아야지만 찾을수있음.
 
 
 result = is_number_exist(3, input)
